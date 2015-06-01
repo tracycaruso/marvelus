c307641c8c1518416598866268e083bae0138bc3
ycartc <tracymcaruso@gmail.com>
2015-06-01 17:51:27 -0600

    Omniauth setup development

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
 
   root to: "home#index"
 
+  get "auth/twitter/callback", to: 'sessions#create'
+  delete "logout", to: 'sessions#destroy'
+  
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
