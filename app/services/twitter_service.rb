3a16fb14b05b0246d345a18e9f57baaaacb52bbb
Mihir Parikh <mihir787@gmail.com>
2015-06-07 19:55:51 -0600

    keys

--- app/services/twitter_service.rb
+++ app/services/twitter_service.rb
@@ -3,10 +3,10 @@ class TwitterService
 
   def initialize
     @twitter = Twitter::REST::Client.new do |config|
-      config.consumer_key =   ENV['TWITTER_CONSUMER_KEY'] #'opXfTHFYAmTjOZrbsJfU1SLH8'
-      config.consumer_secret =   ENV['TWITTER_CONSUMER_SECRET'] #'NJMjgWp26HCNRelzMmBT3v0ho3O11MRDlFd9GT99vVx1hHBXzx'
-      config.access_token =   ENV['TWITTER_YOUR_ACCESS_TOKEN'] #'3232966190-s81bwnpNKpcUueN9GnSRpp9rj6nkqZE3gfgbf1v'
-      config.access_token_secret =  ENV['TWITTER_YOUR_ACCESS_SECRET'] #'PfIn4kdWIODFYBVH9EtPsv1CLNVnAGom8lcWplegd6oH8'
+      config.consumer_key =   ENV['TWITTER_CONSUMER_KEY']
+      config.consumer_secret =   ENV['TWITTER_CONSUMER_SECRET']
+      config.access_token =   ENV['TWITTER_YOUR_ACCESS_TOKEN']
+      config.access_token_secret =  ENV['TWITTER_YOUR_ACCESS_SECRET']
     end
   end
 
@@ -20,5 +20,3 @@ class TwitterService
     twitter.user_timeline(user, count: 1).first.text
   end
 end
-
-  
