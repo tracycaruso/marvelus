86a80d7de655a636ddcfa5cf97d842d4d840dbb7
ycartc <tracymcaruso@gmail.com>
2015-06-10 22:48:30 -0600

    finalized html & css

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
 
   root to: "home#index"
-
+  get "/about", to: "home#about", as: :about
+  
   get "auth/twitter/callback", to: 'sessions#create'
   delete "logout", to: 'sessions#destroy'
-  
+
   resource :user
 
   post "tweet", to: 'users#tweet'
