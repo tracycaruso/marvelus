03115dc66326ff8101afb40f5dad149179efc2ae
Mihir Parikh <mihir787@gmail.com>
2015-06-11 08:17:02 -0600

    fixing tests

--- spec/features/user_sees_their_tweet_on_the_user_page_spec.rb
+++ spec/features/user_sees_their_tweet_on_the_user_page_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe "logged in user" do 
-  before(:each) do 
+RSpec.describe "logged in user" do
+  before(:each) do
     OmniAuth.config.mock_auth[:twitter] = nil
   end
 
@@ -11,9 +11,14 @@ RSpec.describe "logged in user" do
     click_link "Login with Twitter"
   end
 
-  it "can see a tweet on user page" do 
+  def create_superhero
+    Superhero.create(Superhero id: 1, deck: "Bruce Wayne, who witnessed the murder of his billi...", descripton: "good", gender: "1", name: "Batman", real_name: "Bruce Wayne", image: "http://static.comicvine.com/uploads/scale_medium/6...", birthday: "Feb 19, 1972", last_name: nil, publisher: "DC Comics", comic_vine_id: "1699", sentiment_score: -6)
+  end
+
+  it "can see a tweet on user page" do
+    create_superhero
     user_logs_in
-    
+
     expect(page).to have_content("I tweeted this!")
   end
 end
